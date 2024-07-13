f161888fcf1abb023b91082d34213627565fa751
Nik V <nvukovlj@gmail.com>
2024-07-13 18:29:44 +0200

    Starting to add user signup, login, logout

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -26,6 +26,12 @@ func (app *application) routes() http.Handler {
 	mux.Post("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippet))
 	mux.Get("/snippet/:id", dynamicMiddleware.ThenFunc(app.showSnippet))
 
+	mux.Get("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm))
+	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser))
+	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm))
+	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser))
+	mux.Post("/user/logout", dynamicMiddleware.ThenFunc(app.logoutUser))
+
 	// Create a file server which serves files out of the "./ui/static" directory
 	// Note that the path given to the http.Dir function is relative to the program
 	// directory root.
